42dcfc662362e7f2ac3f83751f9454beb2e134a7
Kris247GB <crist.tapiap@duocuc.cl>
2025-11-27 01:32:36 -0300

    Initial commit

---
+++ src/test/java/com/levelup/levelupapi/LevelupapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.levelup.levelupapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LevelupapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
