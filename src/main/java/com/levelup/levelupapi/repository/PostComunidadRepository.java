2d50ea53152ba786f6e248db1125cb0ff9774020
Kris247GB <crist.tapiap@duocuc.cl>
2025-11-30 17:39:05 -0300

    PostComunidadRepository agregado

---
+++ src/main/java/com/levelup/levelupapi/repository/PostComunidadRepository.java
@@ -0,0 +1,7 @@
+package com.levelup.levelupapi.repository;
+
+import com.levelup.levelupapi.model.PostComunidad;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostComunidadRepository extends JpaRepository<PostComunidad, Long> {
+}
