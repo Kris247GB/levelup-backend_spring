2e10c8d72e29880c21a11ec7ce368956b02f343e
Kris247GB <crist.tapiap@duocuc.cl>
2025-11-30 23:58:36 -0300

    imagenes agregadas

--- src/main/java/com/levelup/levelupapi/DataLoader.java
+++ src/main/java/com/levelup/levelupapi/DataLoader.java
@@ -24,7 +24,7 @@ public class DataLoader implements CommandLineRunner {
             producto1.setPrecio(49999);
             producto1.setCategoria("Accesorios");
             producto1.setDescripcion("Control inalámbrico Xbox para PC");
-            producto1.setImagen("link_to_image");
+            producto1.setImagen("https://cdn.gameplanet.com/wp-content/uploads/2022/09/03173440/jsajkdh.jpg");
             producto1.setStock(10);
             producto1.setDestacado(true);
 
@@ -34,7 +34,7 @@ public class DataLoader implements CommandLineRunner {
             producto2.setPrecio(79999);
             producto2.setCategoria("Audio");
             producto2.setDescripcion("Auriculares con micrófono HyperX");
-            producto2.setImagen("link_to_image");
+            producto2.setImagen("https://m.media-amazon.com/images/I/61Qke4HY+QL._AC_.jpg");
             producto2.setStock(15);
             producto2.setDestacado(true);
 
@@ -43,19 +43,18 @@ public class DataLoader implements CommandLineRunner {
             producto3.setNombre("Ratón Gamer");
             producto3.setPrecio(39999);
             producto3.setCategoria("Accesorios");
-            producto3.setDescripcion("Razer DeathAdder V2");
-            producto3.setImagen("link_to_image");
+            producto3.setDescripcion("Mouse Razer Gamer");
+            producto3.setImagen("https://cdn.mos.cms.futurecdn.net/oKG8af4ZbJqjttMd2jKGzZ.jpg");
             producto3.setStock(20);
             producto3.setDestacado(false);
 
-            // Nuevos productos de prueba
             Producto producto4 = new Producto();
             producto4.setCodigo("P004");
             producto4.setNombre("Catan");
             producto4.setPrecio(39999);
             producto4.setCategoria("Juegos de mesa");
             producto4.setDescripcion("Catan, el juego de estrategia para 3-4 jugadores.");
-            producto4.setImagen("link_to_image");
+            producto4.setImagen("https://cdnx.jumpseller.com/gaming-place/image/21439308/auge_juegos_de_mesa-blog2.jpg.jpg?1641168184");
             producto4.setStock(30);
             producto4.setDestacado(true);
 
@@ -65,7 +64,7 @@ public class DataLoader implements CommandLineRunner {
             producto5.setPrecio(499999);
             producto5.setCategoria("Consolas");
             producto5.setDescripcion("PS5, la consola de última generación de Sony.");
-            producto5.setImagen("link_to_image");
+            producto5.setImagen("https://www.notebookcheck.org/fileadmin/Notebooks/News/_nc3/PA5.png");
             producto5.setStock(10);
             producto5.setDestacado(true);
 
@@ -75,7 +74,7 @@ public class DataLoader implements CommandLineRunner {
             producto6.setPrecio(999999);
             producto6.setCategoria("PC Gamer");
             producto6.setDescripcion("PC Gamer potente de ASUS, ideal para juegos de alto rendimiento.");
-            producto6.setImagen("link_to_image");
+            producto6.setImagen("https://i.pinimg.com/originals/12/6a/09/126a09095b9cf1f06259689893d3e153.jpg");
             producto6.setStock(5);
             producto6.setDestacado(true);
 
@@ -85,7 +84,7 @@ public class DataLoader implements CommandLineRunner {
             producto7.setPrecio(79999);
             producto7.setCategoria("Accesorios");
             producto7.setDescripcion("Silla ergonómica de alta calidad para largas sesiones de juego.");
-            producto7.setImagen("link_to_image");
+            producto7.setImagen("https://images.secretlab.co/theme/common/social_meta_og_titan_evo_2022_series-min.jpg");
             producto7.setStock(20);
             producto7.setDestacado(false);
 
@@ -95,7 +94,7 @@ public class DataLoader implements CommandLineRunner {
             producto8.setPrecio(59999);
             producto8.setCategoria("Accesorios");
             producto8.setDescripcion("Mouse gamer con precisión de hasta 16,000 DPI.");
-            producto8.setImagen("link_to_image");
+            producto8.setImagen("https://ctronicmgta.com/wp/wp-content/uploads/2021/03/71O23Hqq3jL._AC_SL1500_.jpg");
             producto8.setStock(50);
             producto8.setDestacado(true);
 
@@ -105,7 +104,7 @@ public class DataLoader implements CommandLineRunner {
             producto9.setPrecio(79999);
             producto9.setCategoria("Audio");
             producto9.setDescripcion("Auriculares con sonido envolvente 7.1 para una experiencia inmersiva.");
-            producto9.setImagen("link_to_image");
+            producto9.setImagen("https://http2.mlstatic.com/D_NQ_NP_699883-MLB47900456340_102021-W.jpg");
             producto9.setStock(40);
             producto9.setDestacado(false);
 
@@ -125,7 +124,7 @@ public class DataLoader implements CommandLineRunner {
             producto11.setPrecio(1000);
             producto11.setCategoria("Comida");
             producto11.setDescripcion("Delicioso dulce chileno, ideal para acompañar con un café.");
-            producto11.setImagen("link_to_image");
+            producto11.setImagen("https://static.theclinic.cl/media/2023/07/14-133943_cib0_Sin-titulo-880x500.jpg");
             producto11.setStock(150);
             producto11.setDestacado(false);
 
@@ -145,7 +144,7 @@ public class DataLoader implements CommandLineRunner {
             producto13.setPrecio(29999);
             producto13.setCategoria("Ropa");
             producto13.setDescripcion("Polera personalizada para gamers.");
-            producto13.setImagen("link_to_image");
+            producto13.setImagen("https://cdnx.jumpseller.com/ters/image/29913986/polera_gamer_jugador_de_videojuegos_juegos_gamer_listo_gameboy_retro_mando_botones_arcade_estampado.png?1669877013");
             producto13.setStock(80);
             producto13.setDestacado(false);
 
@@ -155,11 +154,10 @@ public class DataLoader implements CommandLineRunner {
             producto14.setPrecio(129999);
             producto14.setCategoria("Accesorios");
             producto14.setDescripcion("Teclado mecánico con retroiluminación RGB.");
-            producto14.setImagen("link_to_image");
+            producto14.setImagen("https://m.media-amazon.com/images/I/71PzW7vZNUL._AC_SL1500_.jpg");
             producto14.setStock(25);
             producto14.setDestacado(true);
 
-            // Guardar los productos en la base de datos
             productoRepository.save(producto1);
             productoRepository.save(producto2);
             productoRepository.save(producto3);
